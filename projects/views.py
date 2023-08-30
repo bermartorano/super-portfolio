48d2c8805c4933bcece0203006d4689c324ee72e
Bernardo Martorano <bernardormartorano@gmail.com>
2023-08-30 20:49:09 -0300

    feat: get do not need auth

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
 
@@ -9,6 +9,11 @@ class ProfileViewSet(ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
 
 class ProjectViewSet(ModelViewSet):
