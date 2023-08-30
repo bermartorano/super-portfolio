52bf0c690e801cda70eb0c67ccf5c2ec7f0a6d67
Bernardo Martorano <bernardormartorano@gmail.com>
2023-08-30 18:59:43 -0300

    feat: profile routes

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    github = models.URLField()
+    linkedin = models.URLField()
+    bio = models.TextField(
+            null=False,
+            blank=False,
+            max_length=500,
+            )
+
+    def __str__(self) -> str:
+        return self.name
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileViewSet(ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsAuthenticated]
